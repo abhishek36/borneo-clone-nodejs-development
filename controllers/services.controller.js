f748d62b3c60f6da18b4cef68d3e02386d428d8d
abhishek36 <akamble9913@gmail.com>
2022-02-04 17:53:06 +0530

    first commit

---
+++ controllers/services.controller.js
@@ -0,0 +1,32 @@
+//import Services model here
+
+const addServices = async (req, res) => {
+    //addServices api logic here
+};
+
+const getAllServicesByCategoryId = async (req, res) => {
+    //getAllServicesByCategoryId api logic here
+};
+
+const getServicesById = async (req, res) => {
+    //getServicesById api logic here
+};
+
+const updateServicesById = async (req, res) => {
+    //updateServicesById api logic here
+};
+
+const deleteServicesById = async (req, res) => {
+    //deleteServicesById api logic here
+};
+
+
+const ServicesController = {
+    addServices,
+    getAllServicesByCategoryId,
+    getServicesById,
+    updateServicesById,
+    deleteServicesById
+};
+
+module.exports = ServicesController;
---
+++ models/category.model.js
@@ -0,0 +1,1 @@
+//serviceCategory model
